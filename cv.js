f0350b7facf798755301f36353e86018a2c37d42
calmel <morvan.calmel@gmail.com>
2018-09-29 15:36:28 +0200

    second commit

---
+++ cv.js
@@ -0,0 +1,14 @@
+function myFunctionFormation() {
+    document.getElementById("demo").innerHTML = "- 2018-2019 : Université Paris 8 : Master 2 Informatique Technologies de l’Hypermédia<br />- 2017-2018 : Université Paris 8 : Master 1 Informatique<br /> - 2013 - 2015 : Cycle Ingénieur - Informatique Électronique Automatique - École ESIEA (Paris et Ivry-sur-seine)<br /> - 2012 - 2013 : Licence d’Informatique : Micro-Informatique et Machines Embarquées - Université Paris 8 (Saint-Denis)<br /> - 2010 - 2012 : BTS Informatique de Gestion : option Développeur d’applications – École européenne ETS (Paris 17 ème ) <br />- 2009 - 2010 : Bac STI – Lycée Charles de Gaulle";
+    document.getElementById("demo").style.color = "red";
+}
+
+function myFunctionExpPro() {
+    document.getElementById("demo").innerHTML = "<ul><li>2 mois | octobre - décembre 2011 : Développeur PHP – Images et Technologies – ( Orgeval ) Développement d’un site Web en PHP/MySQL : Evènements, calendrier, création d'articles, API météo, partage de fichiers, pour la mairie de Villevaudé:[ www.villevaude.fr ]</li><li>2 mois | mai - juin 2013 : Développeur mobile – Agence digitale LaNouvelle R – ( Paris 17ème ) Développement d’une application mobile sous Android 4.0 (Eclipse) : FranceExpatriés</li><li>4 mois | mai - août 2015 : Développeur PHP – UNAPEI – ( Paris 18ème ) Maintenance d’un site Web en PHP/MySQL : une cartographie nationale en HTML5 ( JS) et C++ pour une maintenance d'emails</li><li>4 mois | mai - août 2018 : Développeur de middlewares en Python et PHP pour Sophia et Autotask</li></ul>";
+    document.getElementById("demo").style.color = "green";
+}
+
+function myFunctionLanguages() {
+    document.getElementById("demo").innerHTML = "<ul><li>Programmation : Java ; SDK Android ; PHP ( + Symfony 3 ) ; HTML5 ( +Bootstrap ) ; C / C++ ; Python ( +Django ) ; JavaScript ; Design patterns ; Modélisations UML ; Méthode Agile ; Méthode SCRUM ; git ; HTML ; XML ; JSON ;</li><li>Base de données : MySQL ; SQLite ; PL/SQL</li><li>Systèmes d’exploitation : GNU-Linux ; Android ; Windows ;</li><li> Logiciels : Lex ; Yacc ; serveur Apache ; MySQLWorkbench ; MySQLserver ; Eclipse ; WAMP ; DevC++; WinDesign ; FileZilla ; Sublime Text ; Talend Big Data ; </li><li> CMS : WordPress ; Drupal ;</li></ul>";
+    document.getElementById("demo").style.color = "blue";
+}
